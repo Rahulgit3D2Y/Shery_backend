e6e458711f6f657e59f8dc1aa32cec63d07e9c0b
Rahulgit3D2Y <rc2504000@gmail.com>
2024-05-05 16:45:22 +0530

    8

--- miniProject/app.js
+++ miniProject/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const userModel = require("./models/user");
 const postModel = require("./models/post");
+const post = require("./models/post");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
@@ -29,6 +30,30 @@ app.get("/profile", isLoggedIn, async (req, res) => {
   res.render("profile", { user });
 });
 
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+
+  await post.save();
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+  res.redirect("/profile");
+});
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
   res.redirect("/login");
