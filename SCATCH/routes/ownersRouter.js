7172989bf2b23d133321eded8542c2f4bd0cf6d2
Rahulgit3D2Y <rc2504000@gmail.com>
2024-06-23 21:37:33 +0530

    23 june

--- SCATCH/routes/ownersRouter.js
+++ SCATCH/routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
 router.get("/", (req, res) => {
   res.send("yo");
+});
+
+if (process.env === "development") {
+  console.log("yo");
+}
+router.post("/create", async (req, res) => {
+  let owners = await ownerModel.find();
+  if (owners.length > 0) {
+    return res.sendStatus(503);
+  }
+
+  let { fullname, email, password } = req.body;
+
+  let createdOwner = await ownerModel.create({
+    fullname,
+    email,
+    password,
+  });
+  res.status(200).send(createdOwner);
 });
 
 module.exports = router;
