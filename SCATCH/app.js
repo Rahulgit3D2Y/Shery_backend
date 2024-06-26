b5fcae6d5ba7d502c535b59c39e3bcf657ce0dcd
Rahulgit3D2Y <rc2504000@gmail.com>
2024-06-26 20:09:48 +0530

    26

--- SCATCH/app.js
+++ SCATCH/app.js
@@ -6,6 +6,8 @@ const db = require("./config/mongoose-connection");
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
 require("dotenv").config();
 
 app.use(express.json());
@@ -13,7 +15,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engin", "ejs");
-
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
--- SCATCH/routes/usersRouter.js
+++ SCATCH/routes/usersRouter.js
@@ -1,36 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require("../models/user-model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { generateToken } = require("../utils/generateToken");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
 router.get("/", (req, res) => {
   res.send("yo");
 });
 
-router.post("/register", (req, res) => {
-  try {
-    let { email, password, fullname } = req.body;
-    bcrpyt.genSalt(10, function (err, salt) {
-      bcrypt.hash(password, salt, async function (err, hash) {
-        if (err) return res.send(err.message);
-        else {
-          let user = await userModel.create({
-            email,
-            password: hash,
-            fullname,
-          });
+router.post("/register", registerUser);
 
-          let token = generateToken(user);
-          res.cookie("token", token);
-          res.send("user created succesully");
-        }
-      });
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
+router.post("/login", loginUser);
+router.get("/logout", logout);
 module.exports = router;
